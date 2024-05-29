f57ce53da2436c1a75a47913d3a043bbf73c5fd5
Rushabhgadhiya9102 <rushabhgadhiya9102@gmail.com>
2024-05-29 20:33:13 +0530

    commited

---
+++ jquery.js
@@ -0,0 +1,22 @@
+$(function() {
+    $('.selector').animatedHeadline({
+        animationType: 'scale'
+    });
+  })
+  
+
+  $('.slider-single').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
+  });
